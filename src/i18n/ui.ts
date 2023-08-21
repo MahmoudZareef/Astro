0dcde5459208f97ac88b978f89523ca4ec66c9b5
MahmoudZareef <Mahmoudzameen@gmail.com>
2023-08-21 19:40:55 +0300

    Multilanguage 2

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+    en: 'English',
+    tr: 'Turkish',
+    ar: 'Arabic',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.mission': 'mission',
+      'nav.twitter': 'Twitter',
+    },
+    tr: {
+      'nav.home': 'ana sayfa',
+      'nav.mission': 'misyon',
+    },
+    ar: {
+        'nav.home': 'الصفحة الرئيسية',
+        'nav.mission': 'مهمة',
+      },
+  } as const;
